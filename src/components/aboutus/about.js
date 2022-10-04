d210433dd25335f942434f0043d006ffc234b989
kanavchawla <kchawla1_be21@thapar.edu>
2022-10-04 23:05:15 +0530

    polygon logo changed link decor changed font changed

--- src/components/aboutus/about.js
+++ src/components/aboutus/about.js
@@ -10,7 +10,7 @@ function About(){
     <div className="card1">
 
     <p className='p1'>
-    HACKTOBERFEST IS FOR EVERYONE. WHETHER IT’S YOUR FIRST TIME—OR YOUR NINTH—IT’S ALMOST TIME TO HACK OUT FOUR PRISTINE PULL/MERGE REQUESTS AND COMPLETE YOUR MISSION FOR OPEN SOURCE. JOIN OTHER MEMBERS OF THE OPEN-SOURCE COMMUNITY ON THE HACKTOBERFEST DISCORD.PEN-SOURCE PROJECTS, MAINTAINED BY COMMUNITY-MINDED CODERS, MAKE THE MODERN INTERNET FUNCTION.
+    Hacktoberfest is for everyone. Whether it’s your first time—or your ninth—it’s almost time to hack out four pristine pull/merge requests and complete your mission for open source. Join other members of the open-source community on the hacktoberfest discord.Pen-source projects, maintained by community-minded coders, make the modern internet function.
     </p>
     </div>
 
