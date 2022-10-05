71ec4f9c80e052b5a8471566fa4ed76c94373c28
MJ-N-J <mrituanjay2003@gmail.com>
2022-10-05 23:22:08 +0530

    Changed About Us content

--- src/components/aboutus/about.js
+++ src/components/aboutus/about.js
@@ -10,8 +10,10 @@ function About(){
     <div className="card1">
 
     <p className='p1'>
-    Hacktoberfest is for everyone. Whether it’s your first time—or your ninth—it’s almost time to hack out four pristine pull/merge requests and complete your mission for open source. Join other members of the open-source community on the hacktoberfest discord.Pen-source projects, maintained by community-minded coders, make the modern internet function.
-    </p>
+    Hacktoberfest 2022 marks the 8th edition of Hacktoberfest, organized by DigitalOcean, a month-long celebration of open-source coding and software. 
+Open source software code is freely distributed to be publicly accessible - available for anyone to see, modify and share the code as they desire.
+Join budding coders and open-source software enthusiasts worldwide during the month by contributing to open-source projects. Everyone is welcome to contribute - be it your first or your fifth! 
+Team OWASP provides five open source projects to which you can display and contribute - Web-Dev Tasks, Tech Think Tank, Algorithm Visualiser, Tab Saver Extension and Backend-automation NPM. These projects are widely available for contribution by people from various tech stacks. Hack out four pristine pull/merge requests and win exciting goodies and swags!     </p>
     </div>
 
     <a href='https://hacktoberfest.com/auth/'  className='btn-special animate__animated animate__fadeInUp'>
